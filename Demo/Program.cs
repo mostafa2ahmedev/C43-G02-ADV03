f4b775d6f9682462e4312c0d49a9c0f204ea6a62
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-02-05 00:39:02 +0200

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Demo
+﻿
+
+namespace Demo
 {
     internal class Program
     {
+
+        #region Q1)	Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+        //public static void ReverseQueueElementsUsingStack( Queue<int> passedQueue)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //    int count = passedQueue.Count;
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+ 
+        //        passedQueue.TryDequeue(out int item);
+        //        stack.Push(item);
+    
+        //    }
+        //    count = stack.Count;
+
+        //    for (int i = 0; i < count; i++)
+        //    {
+        //        stack.TryPop(out int value);
+        //        passedQueue.Enqueue(value);
+
+        //    }
+
+        //}
+
+
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1)	Given a Queue, implement a function to reverse the elements of a queue using a stack.
+            //Queue<int> queue = new Queue<int>();
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    queue.Enqueue(i+1);
+            //}
+       
+
+
+            //Console.WriteLine("***********");
+ 
+
+            //ReverseQueueElementsUsingStack(queue);
+
+            //int count = queue.Count;
+
+            //for (int i = 0; i < count; i++) 
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //}
+
+            #endregion
         }
     }
 }
